61efa35ab9c929a94bc7f068c84269eab97edf40
Elaha100 <elahasultaniii@gmail.com>
2026-01-22 22:24:51 +0100

    Reorganize SQL

--- 07_curd_delete.sql
+++ 07_curd_delete.sql
@@ -1,12 +1,11 @@
 USE MiniHandelDB;
 GO
 
--- Ta bort en orderrad (orderitem)
 DELETE FROM dbo.OrderItems
 WHERE OrderItemId = 1;
 GO
 
--- Ta bort en betalning
 DELETE FROM dbo.Payments
 WHERE PaymentId = 20;
+
 GO
